79d79b0d8b3bb8d497ca4c777b1de60cdcdc5349
nick <nicklos550@gmail.com>
2025-03-17 00:24:25 +0200

    Handle case where uploaded_at is NULL

--- purchases_data_mart.sql
+++ purchases_data_mart.sql
@@ -1,5 +1,5 @@
 WITH LatestUpload AS (
-    SELECT MAX(uploaded_at) AS latest_upload
+    SELECT COALESCE(MAX(uploaded_at), '1970-01-01 00:00:00'::timestamp) AS latest_upload
     FROM purchases
 )
 SELECT
@@ -9,6 +9,9 @@ SELECT
 FROM orders
 LEFT JOIN transactions ON orders.id = transactions.order_id
 LEFT JOIN verification ON transactions.id = verification.transaction_id
-WHERE orders.uploaded_at > (SELECT latest_upload FROM LatestUpload)
-   OR transactions.uploaded_at > (SELECT latest_upload FROM LatestUpload)
-   OR verification.uploaded_at > (SELECT latest_upload FROM LatestUpload);
+WHERE
+    (orders.uploaded_at > (SELECT latest_upload FROM LatestUpload) OR orders.uploaded_at IS NULL)
+    OR
+    (transactions.uploaded_at > (SELECT latest_upload FROM LatestUpload) OR transactions.uploaded_at IS NULL)
+    OR
+    (verification.uploaded_at > (SELECT latest_upload FROM LatestUpload) OR verification.uploaded_at IS NULL);
