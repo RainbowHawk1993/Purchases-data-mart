f46bbaa07d36fc04d3fec2707be254398102fff7
nick <nicklos550@gmail.com>
2025-03-17 00:17:31 +0200

    init

---
+++ purchases_data_mart.sql
@@ -0,0 +1,14 @@
+WITH LatestUpload AS (
+    SELECT MAX(uploaded_at) AS latest_upload
+    FROM purchases
+)
+SELECT
+    orders.*,
+    transactions.*,
+    verification.*
+FROM orders
+LEFT JOIN transactions ON orders.id = transactions.order_id
+LEFT JOIN verification ON transactions.id = verification.transaction_id
+WHERE orders.uploaded_at > (SELECT latest_upload FROM LatestUpload)
+   OR transactions.uploaded_at > (SELECT latest_upload FROM LatestUpload)
+   OR verification.uploaded_at > (SELECT latest_upload FROM LatestUpload);
